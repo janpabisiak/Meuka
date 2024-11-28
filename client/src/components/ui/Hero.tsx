d1b745bdd8ac26bdf40d582a237d20b0db70d440
janpabisiak <contact@janpabisiak.com>
2024-11-28 01:35:24 +0100

    Update Hero.tsx

--- client/src/components/ui/Hero.tsx
+++ client/src/components/ui/Hero.tsx
@@ -3,12 +3,12 @@ function Hero() {
 		<section className="hero">
 			<div className="hero__item item-1">
 				<h1 className="hero__item__title">
-					Become a <span className="span">fashion</span> person
+					Become a <span className="span">better</span> person
 				</h1>
 			</div>
 			<div className="hero__item item-2">
 				<h1 className="hero__item__title">
-					Become a <span className="span">fashion</span> person
+					Become a <span className="span">better</span> person
 				</h1>
 			</div>
 		</section>
