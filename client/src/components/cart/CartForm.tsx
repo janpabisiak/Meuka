1a08acfb08c9dad25b88e05eefcaf87996b8e3cc
janpabisiak <contact@janpabisiak.com>
2024-11-29 00:06:10 +0100

    react-hook-form moved from cart to cartform component

--- client/src/components/cart/CartForm.tsx
+++ client/src/components/cart/CartForm.tsx
@@ -1,32 +1,86 @@
-import { Link } from 'react-router-dom';
-import { UseFormHandleSubmit, UseFormRegister } from 'react-hook-form';
+import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
 import Button from '../ui/Button';
 import { useUser } from '../../contexts/userContext';
 import ICartFormInputs from '../../interfaces/ICartFormInputs';
+import FormErrors from '../ui/FormErrors';
+import IError from '../../interfaces/IError';
+import ICartProduct from '../../interfaces/ICartProduct';
+import sendRequest from '../../utils/sendRequest';
 
-function CartForm({
-	register,
-	handleSubmit,
-	onSubmit,
-}: {
-	register: UseFormRegister<ICartFormInputs>;
-	handleSubmit: UseFormHandleSubmit<ICartFormInputs>;
-	onSubmit: (data: ICartFormInputs) => void;
-}) {
+interface Props {
+	selectedProducts: ICartProduct[];
+	total: number;
+}
+
+function CartForm({ selectedProducts, total }: Props) {
 	const {
-		state: { isAuthenticated },
+		register,
+		formState: { errors },
+		setValue,
+		handleSubmit,
+	} = useForm<ICartFormInputs>();
+	const navigate = useNavigate();
+	const {
+		state: { firstName, lastName, isAuthenticated },
 		dispatch,
 	} = useUser();
+
+	async function onSubmit(data: ICartFormInputs) {
+		try {
+			const { firstName, lastName, address, city, country } = data;
+			const body = {
+				firstName,
+				lastName,
+				address,
+				city,
+				country,
+				products: selectedProducts,
+				total,
+			};
+
+			await sendRequest({ route: '/orders', method: 'post', token: String(localStorage.getItem('token')), body });
+			dispatch({ type: 'cart/reset' });
+			dispatch({ type: 'sync', payload: true });
+			toast.success('Order successfully created.');
+			navigate('../orders');
+		} catch (err) {
+			if (err instanceof AxiosError) toast.error(err.message);
+			else toast.error('Failed to create order.');
+		}
+	}
+
+	// Set form values if user is logged in
+	useEffect(() => {
+		if (firstName && lastName) {
+			setValue('firstName', firstName);
+			setValue('lastName', lastName);
+		}
+	}, [firstName, lastName, setValue]);
 
 	if (isAuthenticated)
 		return (
 			<form className="form" onSubmit={handleSubmit(onSubmit)}>
 				<h3 className="form__title">Edit delivery data</h3>
-				<input className="input" type="text" placeholder="First name" {...register('firstName', { required: true })} />
-				<input className="input" type="text" placeholder="Last name" {...register('lastName', { required: true })} />
-				<input className="input" type="text" placeholder="Address" {...register('address', { required: true })} />
-				<input className="input" type="text" placeholder="City" {...register('city', { required: true })} />
-				<input className="input" type="text" placeholder="Country" {...register('country', { required: true })} />
+				<FormErrors errors={errors as IError} />
+				<input
+					className="input"
+					type="text"
+					placeholder="First name"
+					{...register('firstName', { required: 'First name is required' })}
+				/>
+				<input
+					className="input"
+					type="text"
+					placeholder="Last name"
+					{...register('lastName', { required: 'Last name is required' })}
+				/>
+				<input className="input" type="text" placeholder="Address" {...register('address', { required: 'Address is required' })} />
+				<input className="input" type="text" placeholder="City" {...register('city', { required: 'City is required' })} />
+				<input className="input" type="text" placeholder="Country" {...register('country', { required: 'Country is required' })} />
 				<Button text="Order" type="submit" />
 				<Button text="Clear cart" isPrimary={false} onClick={() => dispatch({ type: 'cart/reset' })} />
 			</form>
--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,21 +1,12 @@
-import toast from 'react-hot-toast';
-import { useEffect, useMemo } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { AxiosError } from 'axios';
+import { useMemo } from 'react';
 import CartList from '../components/cart/CartList';
 import CartForm from '../components/cart/CartForm';
-import sendRequest from '../utils/sendRequest';
 import { useUser } from '../contexts/userContext';
 import { useProduct } from '../contexts/productContext';
-import ICartFormInputs from '../interfaces/ICartFormInputs';
 
 function Cart() {
-	const { register, setValue, handleSubmit } = useForm<ICartFormInputs>();
-	const navigate = useNavigate();
 	const {
-		state: { firstName, lastName, cart },
-		dispatch,
+		state: { cart },
 	} = useUser();
 	const {
 		state: { products },
@@ -39,40 +30,9 @@ function Cart() {
 	// Calculate total price of cart
 	const total = useMemo(() => parseFloat(selectedProducts.reduce((acc, cur) => acc + cur!.price, 0).toFixed(2)), [selectedProducts]);
 
-	async function onSubmit(data: ICartFormInputs) {
-		try {
-			const { firstName, lastName, address, city, country } = data;
-			const body = {
-				firstName,
-				lastName,
-				address,
-				city,
-				country,
-				products: selectedProducts,
-				total,
-			};
-
-			await sendRequest({ route: '/orders', method: 'post', token: String(localStorage.getItem('token')), body });
-			dispatch({ type: 'cart/reset' });
-			toast.success('Order successfully created.');
-			navigate('../');
-		} catch (err) {
-			if (err instanceof AxiosError) toast.error(err.message);
-			else toast.error('Failed to create order.');
-		}
-	}
-
-	// Set form values if user is logged in
-	useEffect(() => {
-		if (firstName && lastName) {
-			setValue('firstName', firstName);
-			setValue('lastName', lastName);
-		}
-	}, [firstName, lastName, setValue]);
-
 	return (
 		<main className="cart">
-			<CartForm register={register} handleSubmit={handleSubmit} onSubmit={onSubmit} />
+			<CartForm selectedProducts={selectedProducts} total={total} />
 			<CartList selectedProducts={selectedProducts} total={total} />
 		</main>
 	);
